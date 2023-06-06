8e84cb5dc222785a77a0844075b6d3bff8d1d47a
GiulioTani <giulio.tani@uniroma1.it>
2023-06-06 09:49:03 +0200

    Recompute with clean data.

--- launcher.sh
+++ launcher.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
-
-for i in {1..9}
-do 
-    DIRECTORY=/mnt/DATA/NonLinearMI/electrode_BLP_band${i}_bin8
-    echo $i $DIRECTORY
-    if [[ ! -d "$DIRECTORY" ]]
-    then
-        mkdir "$DIRECTORY"
-        cp /mnt/DATA/NonLinearMI/electrode_BLP_band1_bin8/[nt]*npy "$DIRECTORY"
-    fi
-    ./cli.py -r $i -d electrode_BLP -b 8 -S; 
+for DATA in FIX_electrode_BLP FIX_source_BLP
+do
+    for i in {1..9}
+    do 
+        DIRECTORY=/mnt/DATA/NonLinearMI/${DATA}_band${i}_bin8
+        echo $i $DIRECTORY
+        if [[ ! -d "$DIRECTORY" ]]
+        then
+            mkdir "$DIRECTORY"
+            cp /mnt/DATA/NonLinearMI/${DATA}_band1_bin8/[nt]*npy "$DIRECTORY"
+        fi
+        /home/raffaelli/nonLinearity/cli.py -r $i -d "${DATA}" -b 8 -S -c config.ini; 
+    done
 done
