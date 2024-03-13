eefe2a43789d2177b87c6cc593bfe3023814005a
Giulio Tani Raffaelli <giulio.tani@uniroma1.it>
2024-03-13 17:13:57 +0100

    Sync useful scripts.

--- launcher.py
+++ launcher.py
@@ -6,10 +6,42 @@ from scipy.io import loadmat
 regions = [10, 30, 50, 70, 100, 150, 200, 230, 270, 300, 350,
            400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950]
 
-# nle = NLE(config_file="config.ini", bins=8, surrogates=99, cache="cache", save_out=20, dataset="eso245_cra_strin",workers=23)
+nle = NLE(config_file="config.ini", bins=8, surrogates=99, verbose=True, cache="cache/", save_out=True, dataset="trimmed_EEG_bands_time", workers=23)
+for t in [1823, 912, 456]:
+    tmp=nle.estimate(display=False, dataset_sub="5", extended_stats=True, compute_shadow="extend", suffix=f"B{t}_Ext", truncate_input=t)
+    print(f"B{t}_Ext")
+    for k in tmp:
+        print(k, np.mean(tmp[k]))
+    tmp=nle.estimate(display=False, dataset_sub="5", extended_stats=True, compute_shadow=True, suffix=f"B{t}_Nor", truncate_input=t)
+    print(f"B{t}_Nor")
+    for k in tmp:
+        print(k, np.mean(tmp[k]))
+
+
+# for band in range(1, 10):
+#     samples = loadmat(f"/home/raffaelli/NonLinearity/NonLinearityData/EEG_el_so_BLP_NEW/trimmed_EEG_fixedTime_band{band}.mat")["EEG"].shape[0]
+#     bins = max(8, int(np.power(samples, 1/3)))
+#     nle = NLE(config_file="config.ini", bins=bins, surrogates=99, verbose=True,
+#               cache="cache/", save_out=True, dataset="trimmed_EEG_bands_time", suffix="Extended", workers=23)
+#     nle.estimate(display=False, dataset_sub=str(band), extended_stats=True, compute_shadow="extend")
+#     del nle
+
+# bands={i:b for i, b in enumerate([[ 1.,  4.], [ 4.,  8.], [ 8., 12.], [12., 15.], [15., 18.], [18., 30.], [30., 44.], [12., 30.], [ 1., 40.]], start=1)}
+# def fs_band(band):
+#     return int(bands[band][1]*2*1.125+0.5)
+
+# for band in range(1,8):
+#     samples=45*fs_band(band)
+#     bins = max(8, int(np.power(samples, 1/3)))
+#     nle = NLE(config_file="config.ini", bins=bins, surrogates=99, verbose=True,
+#                 cache="cache/", save_out=False, dataset="trimmed_EEG_bands_time", suffix="Cutting", workers=23)
+#     nle.estimate(display=False, dataset_sub="7", extended_stats=True, compute_shadow=True, truncate_input=samples)
+#     del nle
+
+# nle = NLE(config_file="config.ini", bins=8, surrogates=99, cache="cache", save_out=20, dataset="shaved_eso245_cra_strin",workers=23, suffix="Extended", verbose=True)
 
 # for region in regions:
-#     nle.estimate(display=False, dataset_sub=str(region), extended_stats=True, compute_shadow=True)
+#     nle.estimate(display=False, dataset_sub=str(region), extended_stats=True, compute_shadow="extend")
 
 # del nle
 
@@ -61,39 +93,39 @@ regions = [10, 30, 50, 70, 100, 150, 200, 230, 270, 300, 350,
 
 # del nle
 
-for kind, save_out in [("electrode", True), ("source", 1)]:
-    print(kind)
-    prefix = "NEW" if kind=="electrode" else "PCA"
-    for av in [1,2,4]:
-        for who in ["", "_shadow"]:
-            if not who and kind=="electrode": #true electrode c'è già
-                continue
+# for kind, save_out in [("electrode", True), ("source", 1)]:
+#     print(kind)
+#     prefix = "NEW" if kind=="electrode" else "PCA"
+#     for av in [1,2,4]:
+#         for who in ["", "_shadow"]:
+#             if not who and kind=="electrode": #true electrode c'è già
+#                 continue
 
-            print(str(av)+who, "\t1", end="\t")
-            samples = loadmat(f"/home/raffaelli/NonLinearity/NonLinearityData/EEG_el_so_BLP_NEW/{prefix}_{kind}BLP_fixedTime_avg{av}{who}_band1.mat")["EEG" if kind=="source" else "BLP"].shape[0]
-            bins = int(np.power(samples, 1/3))
+#             print(str(av)+who, "\t1", end="\t")
+#             samples = loadmat(f"/home/raffaelli/NonLinearity/NonLinearityData/EEG_el_so_BLP_NEW/{prefix}_{kind}BLP_fixedTime_avg{av}{who}_band1.mat")["EEG" if kind=="source" else "BLP"].shape[0]
+#             bins = int(np.power(samples, 1/3))
 
-            nle = NLE(config_file="config.ini", bins=bins, surrogates=99, cache="cache", save_out=save_out, dataset=f"{prefix}_{kind}BLP_time_avg{av}{who}", workers=23, verbose=True)
-            for band in range(1, 10):
-                if av==1 and kind=="electrode": #avg1 c'è già
-                    continue
-                nle.estimate(display=False, dataset_sub=str(band),
-                            extended_stats=True, compute_shadow=(not who))
-                nle.verbose = False
-            del nle
-            if av == 4:
-                print()
-                continue
+#             nle = NLE(config_file="config.ini", bins=bins, surrogates=99, cache="cache", save_out=save_out, dataset=f"{prefix}_{kind}BLP_time_avg{av}{who}", workers=23, verbose=True)
+#             for band in range(1, 10):
+#                 if av==1 and kind=="electrode": #avg1 c'è già
+#                     continue
+#                 nle.estimate(display=False, dataset_sub=str(band),
+#                             extended_stats=True, compute_shadow=(not who))
+#                 nle.verbose = False
+#             del nle
+#             if av == 4:
+#                 print()
+#                 continue
 
-            print(" 2")
-            nle = NLE(config_file="config.ini", bins=6, surrogates=99, cache="cache", save_out=save_out, dataset=f"{prefix}_{kind}BLP_samples_avg{av}{who}", workers=23, verbose=True)
+#             print(" 2")
+#             nle = NLE(config_file="config.ini", bins=6, surrogates=99, cache="cache", save_out=save_out, dataset=f"{prefix}_{kind}BLP_samples_avg{av}{who}", workers=23, verbose=True)
 
-            for band in range(1, 10):
-                nle.estimate(display=False, dataset_sub=str(band),
-                            extended_stats=True, compute_shadow=(not who))
-                nle.verbose = False
+#             for band in range(1, 10):
+#                 nle.estimate(display=False, dataset_sub=str(band),
+#                             extended_stats=True, compute_shadow=(not who))
+#                 nle.verbose = False
 
-            del nle
+#             del nle
 
 
 # %%
---
+++ notebooks/extractPieces.py
@@ -0,0 +1,52 @@
+import io, h5py, zipfile
+import numpy as np
+from tqdm import tqdm
+import pickle
+from scipy import signal
+
+bands={i:b for i, b in enumerate([[ 1.,  4.], [ 4.,  8.], [ 8., 12.], [12., 15.], [15., 18.], [18., 30.], [30., 44.], [12., 30.], [ 1., 40.]], start=1)}
+
+basePath = "../NonLinearityData/EEG_el_so_BLP_NEW"
+f_sample = 1000
+fs_under = 120
+
+def fs_band(band):
+    return int(bands[band][1]*2*1.125+0.5)
+
+for band in range(9):
+    with open(f"auxiliary_data/new_pieces_band{band+1}.pickle", "wb") as fp:
+        pass
+
+zip_file = zipfile.PyZipFile("../NonLinearityData/EEG_el_so_BLP_20230714.zip")
+new_pieces = [[[] for subject in range(50)] for band in range(9)]
+for subject in tqdm(range(50), desc="Subject"):
+    archive = h5py.File(io.BytesIO(zip_file.read(f"Sub_{subject+1}/EEG_bands.mat")))
+
+    for band in tqdm(bands, desc="Band", total=9):
+        band_data = archive[archive["EEG_bands"][0,band-1]]
+        fs_dest = fs_band(band)
+        needed_samples = max(1000, 45*fs_dest)
+        pieces = []
+        tot_len = 0
+
+        for i in range(band_data.shape[0]):
+            this_piece = archive[band_data[i,0]]
+            chunks = int(this_piece.shape[0]/f_sample)
+
+            if chunks == 0:
+                continue
+
+            tot_samples_dest = chunks * fs_dest
+            border = max(1, tot_samples_dest//20)
+            tot_len += tot_samples_dest - 2*border
+            
+            tot_samples_under = chunks * fs_under
+            blaf = signal.resample(this_piece[:f_sample*chunks,:], tot_samples_under)
+            new_pieces[band-1][subject].append(blaf)
+            if tot_len > needed_samples:
+                break
+
+for band in range(9):
+    with open(f"auxiliary_data/new_pieces_band{band+1}.pickle", "wb") as fp:
+        pickle.dump(new_pieces[band], fp)
+
